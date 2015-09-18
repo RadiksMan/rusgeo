a130549f72f46441cc1658aec1dd0ffb21c5cf17
Radion <radion91@gmail.com>
2015-09-18 19:50:03 +0300

    add page (

---
+++ services.php
@@ -0,0 +1,52 @@
+<div class="information">
+    <div class="content-top-wrap cfix">
+        <div class="mbox">
+            <div class="right-column-wrap">
+                <div class="blurb-block-wrap">
+                    <a href="#">
+                        <img src="images/blurb-img-1.png" alt="" />
+                    </a>
+                </div>
+                <div class="blurb-block-wrap">
+                    <a href="#">
+                        <img src="images/blurb-img-2.png" alt="" />
+                    </a>
+                </div>
+            </div>
+            <div class="left-content-wrap">
+                <div class="information_wrap">
+                    <div class="title">
+                        Услуги
+                    </div>
+                    <div class="information_row_wrap">
+                        <div class="information_row">
+                            <a href="#">
+                                <div class="information_title">Аренда</div>
+                                <div class="information_text">Аренда геодезического оборудования в компании "РУСГЕОКОМ" позволит Вам не приобретать дорогостоящее оборудование, а использовать его только по мере необходимости. Помимо прочего, аренда геодезического оборудования позволит снизить стоимость основных средств и поможет платить меньше налогов.</div>
+                            </a>
+                        </div>
+                        <div class="information_row">
+                            <a href="#">
+                                <div class="information_title">Ремонт</div>
+                                <div class="information_text">Ремонт геодезического оборудования, включая их гарантийное и постгарантийное сервисное обслуживание - одно из направлений деятельности компании "РУСГЕОКОМ". Специалисты нашего сервисного центра производят профессиональный ремонт геодезических приборов любого типа, в том числе оптических, лазерных и цифровых нивелиров, оптических и цифровых теодолитов...</div>
+                            </a>
+                        </div>
+                        <div class="information_row">
+                            <a href="#">
+                                <div class="information_title">Поверка</div>
+                                <div class="information_text">Поверка измерительных приборов – это очень важное мероприятие, ведь от профессионального и своевременного метрологического обеспечения ваших инструментов зависит точность и надежность ваших измерений, каким бы видом деятельности вы ни занимались.</div>
+                            </a>
+                        </div>
+                        <div class="information_row">
+                            <a href="#">
+                                <div class="information_title">Trade-in</div>
+                                <div class="information_text">Мы поможем обновить ваш парк измерительного оборудования без значительных затрат и в минимальные сроки. Избавьтесь от вышедшей из строя или устаревшей техники, получите компенсацию и приобретите новые приборы по программе Trade In.</div>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
